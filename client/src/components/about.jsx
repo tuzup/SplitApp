29260c459c2ab9b56cb096f61eb888ab3590506e
tuzup <sunny.george@live.com>
2022-07-27 12:54:38 +0530

    About update

--- client/src/components/about.jsx
+++ client/src/components/about.jsx
@@ -124,9 +124,13 @@ to keep improving the project.</p>
 <h2 id="license">License</h2>
 <br/>
 <p>This project is MIT licensed.</p>
+<br/>
 <p>Copyright 2022 Sunny G Vaikathuparampan</p>
+<br/>
 <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the &quot;Software&quot;), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+<br/>
 <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+<br/>
 <p>THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.</p>
 
 </Container>
