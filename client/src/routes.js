7d6afeddcbd1e026ed839ea27b591a2fc27c5a31
tuzup <sunny.george@live.com>
2022-07-27 12:42:33 +0530

    Added about page

--- client/src/routes.js
+++ client/src/routes.js
@@ -21,6 +21,7 @@ import Dashboard from './components/dashboard';
 import { ViewExpense } from './components/expense/viewExpense';
 import EditExpense from './components/expense/editExpense';
 import { EditGroup } from './components/groups/editGroup';
+import About from './components/about';
 
 
 export default function Router() {
@@ -45,7 +46,8 @@ export default function Router() {
           element: <ViewGroup/>},
         {path:configData.EDIT_GROUP_ROUTER_URL,
           element: <EditGroup/>},
-        {path:configData.ABOUT_URL},
+        {path:configData.ABOUT_URL,
+          element: <About/>},
         {path:configData.USER_PROFILE_URL,
           element: <Profile/>}
       ]
